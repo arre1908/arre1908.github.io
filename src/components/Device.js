f4b8bc95ac5edb64e968f6a2617c139bc03ebab2
arre1908 <arredoluis@gmail.com>
2020-08-19 22:55:39 -0500

    Created initial components and styles of website.

---
+++ src/components/Device.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Screen from "./Screen";
+
+function Device() {
+  return (
+    <div className="device">
+      <Screen />
+    </div>
+  );
+}
+
+export default Device;
---
+++ src/components/Screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Screen() {
+  return (
+    <div className="screen">
+      <div>
+        <code>
+          <span className="text-accent">me@luisarredondo.com</span>:~$ _
+        </code>
+      </div>
+    </div>
+  );
+}
+
+export default Screen;
