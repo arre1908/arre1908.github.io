a49626d0583b5a56005acab20111eedbfdedf01d
arre1908 <arredoluis@gmail.com>
2020-08-25 14:53:04 -0500

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# arre1908.github.io
-
-Personal website made entirely with HTML/CSS
-
-Desktop and mobile supported
---
+++ precache-manifest.22a0ba8e15f76737c2631c11236c3e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8c1d9eea611510b7dd433aab31b864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ccd62616bd623044cc",
+    "url": "/static/css/main.f93ed09b.chunk.css"
+  },
+  {
+    "revision": "16086a82d14d174bd042",
+    "url": "/static/js/2.cda25f67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cda25f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ccd62616bd623044cc",
+    "url": "/static/js/main.fe672f84.chunk.js"
+  },
+  {
+    "revision": "98f921a785448c56e992",
+    "url": "/static/js/runtime-main.3fcc547a.js"
+  }
+]);
