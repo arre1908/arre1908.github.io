b5a2aeccf8cf44b726e72eeab437e17008933d3c
arre1908 <arredoluis@gmail.com>
2020-11-03 17:03:29 -0600

    gtihub link update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const links = {
   resume:
     "https://docs.google.com/document/d/1TEsW382Kkjgz4FqoFjjwV-o1kY27UScMC7JrAfnsfoo/edit?usp=sharing",
   linkedin: "https://linkedin.com/in/luis-arredondo-262067115/",
-  github: "https://github.com/arre1908/arre1908.github.io/",
+  github: "https://github.com/arre1908",
   email: "arredoluis@gmail.com",
   swapi: "https://luisarredondo.com/vue-swapi",
 };
